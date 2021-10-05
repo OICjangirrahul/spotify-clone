a4de5ff740adf6257b3b1db775d8c23d3653bcce
Rahul jangir <84994136+OICjangirrahul@users.noreply.github.com>
2021-10-05 21:23:15 +0900

    list

---
+++ lib/services/music_operations.dart
@@ -0,0 +1,19 @@
+import 'package:spotify/models/music.dart';
+
+class MusicOperations{
+    MusicOperations._(){}
+      static List<Music>getMusic(){
+        return <Music>[
+         Music('ae dil', 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/0e/09/c4/0e09c462-c0cd-0a6c-d748-ea69b70442b7/source/256x256bb.jpg', 'ae dil h mushkil'),
+         Music('ae dil', 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/0e/09/c4/0e09c462-c0cd-0a6c-d748-ea69b70442b7/source/256x256bb.jpg', 'ae dil h mushkil'), 
+         Music('ae dil', 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/0e/09/c4/0e09c462-c0cd-0a6c-d748-ea69b70442b7/source/256x256bb.jpg', 'ae dil h mushkil'),
+         Music('ae dil', 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/0e/09/c4/0e09c462-c0cd-0a6c-d748-ea69b70442b7/source/256x256bb.jpg', 'ae dil h mushkil'), 
+         Music('ae dil', 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/0e/09/c4/0e09c462-c0cd-0a6c-d748-ea69b70442b7/source/256x256bb.jpg', 'ae dil h mushkil'),
+         Music('ae dil', 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/0e/09/c4/0e09c462-c0cd-0a6c-d748-ea69b70442b7/source/256x256bb.jpg', 'ae dil h mushkil'), 
+         Music('ae dil', 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/0e/09/c4/0e09c462-c0cd-0a6c-d748-ea69b70442b7/source/256x256bb.jpg', 'ae dil h mushkil'),
+         Music('ae dil', 'https://is1-ssl.mzstatic.com/image/thumb/Purple123/v4/0e/09/c4/0e09c462-c0cd-0a6c-d748-ea69b70442b7/source/256x256bb.jpg', 'ae dil h mushkil'),       
+        
+
+        ];
+      }
+    }
